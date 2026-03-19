9450823a0b66b75df07e3230ea1b3e7aa05c7dc7
AdanH <hassual@gmail.com>
2026-03-19 16:32:13 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,6 @@
 import { defineConfig } from "astro/config";
-import icon from "astro-icon";
-import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
 
-import tailwindcss from "@tailwindcss/vite";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
-  output: "server",
-
-  adapter: node({
-    mode: "standalone",
-  }),
-
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: "https://AdanH.github.io",
+  base: "/Rat-Review",
 });
