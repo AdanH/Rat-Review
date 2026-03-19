1640330d776a73d2149a95df7c873184b3fee756
AdanH <hassual@gmail.com>
2026-03-19 16:35:55 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 export default defineConfig({
+  integrations: [icon()],
   site: "https://AdanH.github.io",
   base: "/Rat-Review",
 });
